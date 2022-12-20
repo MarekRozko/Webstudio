933903747d4e53a357feec140eab06e9ecc3cf9d
Marek Rożko <maknamar12@gmail.com>
2022-12-20 14:45:48 +0100

    add fixed

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,100 +1,133 @@
-
-
 class Portfolio {
-    projects = [
-        {
-            img: './images/techno.jpeg',
-            title: 'techno',
-            catefory: 1,
-        },
-        {
-            img: './images/techno.jpeg',
-            title: 'pposter',
-            catefory: 3,
-        },
-        {
-            img: './images/techno.jpeg',
-            title: 'restorant',
-            catefory: 2,
-        },
-        {
-            img: './images/techno.jpeg',
-            title: 'ad',
-            catefory: 4,
-        },
-    ];
-
-    categories = [
+  projects = [
     {
-        id: 1,
-        name: 'web',
+      img: "./images/techno.jpeg",
+      title: "Технокряк",
+      catefory: 1,
     },
     {
-        id: 2,
-        name: 'app',
+      img: "./images/poster.jpeg",
+      title: "Постер New Orlean vs Golden Star",
+      catefory: 3,
     },
     {
-        id: 3,
-        name: 'design',
+      img: "./images/restuarent.jpeg",
+      title: "Ресторан Seafood",
+      catefory: 2,
     },
     {
-        id: 4,
-        name: 'marketing',
+      img: "./images/headphones.jpeg",
+      title: "Проект Prime",
+      catefory: 4,
     },
-    ];
-    
-    currentCategory = 0;
+    {
+      img: "./images/boxes.jpeg",
+      title: "Проект Boxes",
+      catefory: 2,
+    },
+    {
+      img: "./images/macbook.jpeg",
+      title: "Inspiration has no Borders",
+      catefory: 1,
+    },
+    {
+      img: "./images/magazine.jpeg",
+      title: "Издание Limited Edition",
+      catefory: 3,
+    },
+    {
+      img: "./images/lable.jpeg",
+      title: "Проект LAB",
+      catefory: 4,
+    },
+    {
+      img: "./images/macbook1.jpeg",
+      title: "Growing Business",
+      catefory: 2,
+    },
+  ];
 
-    galleryListElem = document.querySelector('.gallery-list');
-    buttons = document.querySelectorAll('.button-set__item');
+  categories = [
+    {
+      id: 1,
+      name: "Веб-сайт",
+    },
+    {
+      id: 2,
+      name: "Приложение",
+    },
+    {
+      id: 3,
+      name: "Дизайн",
+    },
+    {
+      id: 4,
+      name: "Маркетинг",
+    },
+  ];
 
-    constructor() {
-        this.render();
-        this.addButtonListeners();
-    }
+  currentCategory = 0;
 
-    addButtonListeners() {
-        this.buttons.forEach(button => button.addEventListener('click', this.onClick.bind(this)))
-    }
+  galleryListElem = document.querySelector(".gallery-list");
+  buttons = document.querySelectorAll(".button-set__item");
 
-    render() {
-        this.galleryListElem.innerHTML = '';
+  constructor() {
+    this.render();
+    this.addButtonListeners();
+  }
 
-        this.projects
-            .filter(project => {
-                if (this.currentCategory === 0) return true;
-                return project.catefory === this.currentCategory;
-            })
-            .forEach(project => {
-                const category = this.categories.find(category => category.id === project.catefory)
-                const li = document.createElement('li');
-                li.classList.add('gallery-list__item')
-                const content = `
-                <a class="gallery-list__link" href="#">
+  addButtonListeners() {
+    this.buttons.forEach((button) =>
+      button.addEventListener("click", this.onClick.bind(this))
+    );
+  }
+
+  render() {
+    this.galleryListElem.innerHTML = "";
+
+    this.projects
+      .filter((project) => {
+        if (this.currentCategory === 0) return true;
+        return project.catefory === this.currentCategory;
+      })
+      .forEach((project) => {
+        const category = this.categories.find(
+          (category) => category.id === project.catefory
+        );
+        const li = document.createElement("li");
+        li.classList.add("gallery-list__item");
+        const content = `
+                <a class="gallery-list__link" href="">
                                 <div class="foto-card">
-                                    <img class="foto-card__img" src="${project.img}" alt="На компьютере изображены картинки" width="370">
+                                    <img class="foto-card__img" src="${
+                                      project.img
+                                    }" alt="На компьютере изображены картинки" width="370">
                                     <p class="foto-card__text list"><span class="foto-card__posicion">Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового
                                     шпионажа и атак на защищённые сервера конкурентов.</span></p>
                                 </div>
                                 <div class="frame">
-                                    <h2 class="frame__subtitle list">${project.title}</h2>
-                                        <p class="frame__text list">${category?.name || '-'}</p>
+                                    <h2 class="frame__subtitle list">${
+                                      project.title
+                                    }</h2>
+                                        <p class="frame__text list">${
+                                          category?.name || "-"
+                                        }</p>
                                 </div>
                                 
                             </a>
                 `;
-                li.innerHTML = content;
+        li.innerHTML = content;
 
-                this.galleryListElem.appendChild(li);
-            });
-    }
+        this.galleryListElem.appendChild(li);
+      });
+  }
 
-    onClick(e) {
-        const button = e.target;
-        const category = button.dataset['category'];
-        this.currentCategory = +category;
-        this.render();
-    }
+  onClick(e) {
+    const button = e.target;
+    const category = button.dataset["category"];
+    this.currentCategory = +category;
+    this.render();
+  }
 }
 
 new Portfolio();
